2945402111a08c5ab56c11d579100f704780e954
jaq-cat <jaq.cat@protonmail.ch>
2021-03-13 20:13:53 -0300

    bettr

--- lib/home.dart
+++ lib/home.dart
@@ -33,8 +33,9 @@ class HomePageState extends State<HomePage> {
                 builder: (context, snapshot) {
                     if (!snapshot.hasData) return Container();
                     return ListView(
-                        children: List<Widget>.from(snapshot.data.keys.map((date) {
-                            double net = snapshot.data[date];
+                        children: List<Widget>.from(snapshot.data.map((pair) {
+                            String date = getDateString(pair.first);
+                            double net = pair.last;
                             return Container(
                                 margin: EdgeInsets.only(
                                     left: widget.margin, top: widget.margin, right: widget.margin),
@@ -53,7 +54,7 @@ class HomePageState extends State<HomePage> {
                                         Row(
                                             children: <Widget>[
                                                 Text(
-                                                    getDateString(date),
+                                                    date,
                                                     style: TextStyle(
                                                         color:  Colors.white24,
                                                     ),
--- lib/infoio.dart
+++ lib/infoio.dart
@@ -1,9 +1,9 @@
-Future<Map<DateTime, double>> getInfo([int limit = 25]) async {
-    return {
-        DateTime.now(): 10.5,
-        DateTime.now(): 0.0,
-        DateTime.now(): -1.2,
-        DateTime.now(): 20.0,
-        DateTime.now(): 2000.0,
-    };
+Future<List<List<dynamic>>> getInfo([int limit = 25]) async {
+    return [
+        [DateTime.now().add(Duration(days: 0)), 10.5],
+        [DateTime.now().add(Duration(days: 1)), 0.0],
+        [DateTime.now().add(Duration(days: 2)), -1.2],
+        [DateTime.now().add(Duration(days: 3)), 20.0],
+        [DateTime.now().add(Duration(days: 4)), 2000.0],
+    ];
 }
