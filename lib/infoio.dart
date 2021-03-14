10ac0fc9782c8c050434dda5858654065b1dec3e
jaq-cat <jaq.cat@protonmail.ch>
2021-03-14 14:04:55 -0300

    working lul, IO left

--- lib/infoio.dart
+++ lib/infoio.dart
@@ -4,7 +4,7 @@ final rng = new Random();
 
 Future<List<List<dynamic>>> getMonthlyInfo() async {
     return List.generate(rng.nextInt(31), (i) =>
-        [DateTime.now().add(Duration(days: i)), (rng.nextDouble() - 0.5) * 100]);
+        [DateTime.now().subtract(Duration(days: i)), (rng.nextDouble() - 0.5) * 100]);
 }
 
 DateTime toMonth(DateTime dt) => DateTime(dt.year, dt.month);
@@ -12,10 +12,10 @@ DateTime toYear(DateTime dt) => DateTime(dt.year);
 
 Future<List<List<dynamic>>> getYearlyInfo() async {
     return List.generate(rng.nextInt(31), (i) =>
-        [toMonth(DateTime.now().add(Duration(days: 31*i))), (rng.nextDouble() - 0.5) * 100]);
+        [toMonth(DateTime.now().subtract(Duration(days: 31*i))), (rng.nextDouble() - 0.5) * 100]);
 }
 
 Future<List<List<dynamic>>> getAllTimeInfo() async {
     return List.generate(rng.nextInt(10), (i) =>
-        [toYear(DateTime.now().subtract(Duration(days: 365))), (rng.nextDouble() - 0.5) * 100]);
+        [toYear(DateTime.now().subtract(Duration(days: 365*i))), (rng.nextDouble() - 0.5) * 100]);
 }
